a93731fecf88f727f294566b16910d9732a00fdc
JackieC1993 <jackiecheung@pursuit.org>
2023-10-29 21:49:39 -0400

    added backend requirement

--- controllers/transactionsController.js
+++ controllers/transactionsController.js
@@ -2,98 +2,117 @@ const express = require('express');
 const transactionRouter = express.Router();
 const transactionArray = require('../models/data');
 
-transactionRouter.use(express.json());
+// transactionRouter.use(express.json());
 
-transactionRouter.get('/', (req,res,next) => {
+// READ Transactions (Index)
+transactionRouter.get('/', (req, res, next) => {
     try{
-        if(transactionArray && transactionArray.length > 0){
-            res.status(200).json(transactionArray);
+        if(transactionArray && transactionArray.length > 0) { 
+            res.status(200).send(transactionArray); 
         }
-        else{
-            res.status(404).json({message: 'No transactions found'});
+        else {
+            res.status(404).send({message: "Could not find transactions"});
         }
+    } 
+    
+    catch(error) {
+        next(error);
     }
-    catch(err){
-        next(err)
-    }
+ 
 });
 
-transactionRouter.get('/:id', (req,res,next) => {
-    try{
-        const id = req.params.id;
-        const transaction = transactionArray.find(item=> item.id === parseInt(id));
+// READ Transactions (Show)
+
+transactionRouter.get('/:id', (req, res, next) => { 
+    try {
+        const id = req.params.id;                           
+        const transaction = transactionArray.find(item => item.id === parseInt(id));
+
         if(transaction){
-            res.status(200).json(transaction);
+            res.status(200).send(transaction);
         }
-        else{
-            res.status(404).json({message: 'Transaction not found'});
+        else {
+            res.status(404).send({message: "Error No Transaction"});
         }
 
     }
-    catch(err){
-        next(err);
+    
+    catch(error){
+        next(error);
     }
+
 });
 
-transactionRouter.post('/', (req,res,next) => {
-    try{
-        const transaction = req.body;
-        if(transaction){
-        transactionArray.push(transaction);
-        res.status(201).json(transaction);
+
+// Create Transactions (Create)
+transactionRouter.post('/', (req,res, next) => { 
+    try {
+        const transactionBody = req.body;
+        if(transactionBody){
+            transactionArray.push(transactionBody);
+            res.status(201).send(transactionBody);
+        }
+        else{
+            res.status(404).send({message: "Transaction was not created"});
+        }
     }
-    else{
-        res.status(404).json({message: 'Transaction not found'});
+
+    catch(error){
+        next(error);
     }
-}
-catch(error){
-    next(error)
-}
 
 });
 
 transactionRouter.put('/:id', (req,res,next) => {
     try{
-        const transactionId = req.body;
-        const transactionToUpdate = req.body;
-        const transactionIndex = transactionArray.findIndex(element=> element.id === transactionId);
+        const transactionId = parseInt(req.params.id);
+        const transactionToUpdate = req.body; 
+        const transactionIndex = transactionArray.findIndex(element => element.id === transactionId);
 
-        if (transactionIndex!== -1) {
-            res.status(404).send({message: "Transaction not found });
+        if(transactionIndex === -1){
+            res.status(404).send({message: "Transaction not found"});
         }
 
+        /// takes the transaction from the db (transactionArray) that matches the index
         const currentTransaction = transactionArray[transactionIndex];
-
-        for (let key in transactionToUpdate) {
-            if (currentTransaction.hasOwnProperty([key])) {
-                currentTransaction[key] = transactionToUpdate[key];
+       
+        /// loop through the transaction that was requested 
+        for(let key in transactionToUpdate) {
+            if(currentTransaction.hasOwnProperty([key])){
+                currentTransaction[key] = transactionToUpdate[key]; ///from the request
             }
         }
+
+        /// updates the transaction in the array of transactions (data.js)
         transactionArray[transactionIndex] = currentTransaction;
 
         res.send(currentTransaction);
     }
-    catch(error){
-        next(error)
+
+    catch(error) {
+        next(error);
     }
-    });
-    
-    transactionRouter.delete('/:id',(req,res,next) => {
-        try{
-            const id = parseInt(req.params.id);
-            const itemIndex = transactionArray.findIndex(element=> element.id === id);
-             if (itemIndex === -1) {
-                return res.status(404).json({message: 'Item not found'});
-             }
 
-             const deletedItem = transactionArray.splice(itemIndex, 1);
-
-             res.send(deletedItem[0]);
-            }catch (error){
-                next(error);
-            }
-        });
-        module.exports = transactionRouter;
+});
 
 
-        
+// DELETE an item by ID
+transactionRouter.delete('/:id', (req, res, next) => {
+    try {
+        const id = parseInt(req.params.id);
+        const itemIndex = transactionArray.findIndex(item => item.id === id);
+
+        if (itemIndex === -1) {
+            return res.status(404).send({ message: 'Item not found' });
+        }
+
+        const deletedItem = transactionArray.splice(itemIndex, 1); 
+
+        res.send(deletedItem[0]);
+    } catch (error) {
+        next(error);
+    }
+});
+
+
+module.exports = transactionRouter
